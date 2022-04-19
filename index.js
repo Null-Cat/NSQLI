98bfecdeaace61137ab5c749db9996e64525c504
Null-Cat <20419682+Null-Cat@users.noreply.github.com>
2022-04-19 23:34:29 +0100

    Added Base SQL Query Functionality

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const express = require('express')
 const app = express()
+
 const port = process.env.PORT || 3000
+
+//Routers
+const sql = require('./routes/sql/sql')
+
+app.use(LogConnections)
+
+app.use(express.urlencoded({ extended: true }))
+app.use('/sql', sql)
+
+app.use(express.static('public'))
+
+function LogConnections(req, res, next) {
+    console.log(`${req.method} request for ${req.url} from ${req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.socket.remoteAddress}`)
+    next()
+}
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
---
+++ routes/sql/dbconfig.js
@@ -0,0 +1,9 @@
+const config = {
+    db: {
+        host: 'db4free.net',
+        user: 'bruhmoment',
+        password: 'YC_R#s2idmb4NNg',
+        database: 'nsqliproj'
+    }
+}
+module.exports = config
