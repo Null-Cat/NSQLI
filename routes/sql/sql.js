b536356c8cadf1a5db74d9dc31f44b39da906589
Null-Cat <20419682+Null-Cat@users.noreply.github.com>
2022-04-20 11:35:50 +0100

    Added Authentication using JWT

--- routes/sql/sql.js
+++ routes/sql/sql.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const mysql = require('mysql')
+const jwt = require('jsonwebtoken')
 
 const dbconfig = require('./dbconfig')
 const con = mysql.createConnection(dbconfig.db)
+const jwtSecret = 'SnowD0wnVERYsEcReTPaSSw0Rd'
 
 const router = express.Router()
+
+router.use(authenticateJWT)
 
 router
     .route('/')
@@ -14,16 +18,19 @@ router
             return
     ***REMOVED***
         if (req.query.request.toLowerCase() === "matchhistory") {
+            con.connect()
             con.query('SELECT * FROM MatchHistory', (err, rows) => {
                 if (err) throw err
                 res.json(rows)
         ***REMOVED***)
+            con.end()
     ***REMOVED*** else {
             res.sendStatus(404)
     ***REMOVED***
 ***REMOVED***)
     .post(express.json(), (req, res) => {
         if (req.body.cmd === 'add') {
+            con.connect()
             con.query('INSERT INTO MatchHistory SET ?', req.body.data, (err, result) => {
                 if (err && err.code === 'ER_DUP_ENTRY') {
                     console.log('Duplicate Entry')
@@ -35,7 +42,25 @@ router
                 console.log('Added New Match')
                 console.log(result)
         ***REMOVED***)
+            con.end()
     ***REMOVED***
 ***REMOVED***)
+
+function authenticateJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1]
+
+        jwt.verify(token, jwtSecret, (err, user) => {
+            if (err) return res.sendStatus(403)
+            console.log(user)
+            req.user = user
+            next()
+    ***REMOVED***)
+***REMOVED*** else {
+        res.sendStatus(401)
+***REMOVED***
+***REMOVED***
 
 module.exports = router
