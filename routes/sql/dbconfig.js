c7c8d36026c9afb70eecbb1fdff1c943c3bb1fbd
Null-Cat <20419682+Null-Cat@users.noreply.github.com>
2022-04-20 11:35:11 +0100

    Added Alternate Database

--- routes/sql/dbconfig.js
+++ routes/sql/dbconfig.js
@@ -1,9 +1,17 @@
+// const config = {
+//     db: {
+//         host: 'db4free.net',
+//         user: 'bruhmoment',
+//         password: 'YC_R#s2idmb4NNg',
+//         database: 'nsqliproj'
+//     }
+// }
 const config = {
     db: {
-        host: 'db4free.net',
-        user: 'bruhmoment',
-        password: 'YC_R#s2idmb4NNg',
-        database: 'nsqliproj'
+        host: 'remotemysql.com',
+        user: 'gniP59cV8F',
+        password: 'xa3VLSoIG4',
+        database: 'gniP59cV8F'
     }
 }
 module.exports = config
