7ecd7fb864080363d3636887d6f0a4323e7ca462
Null-Cat <20419682+Null-Cat@users.noreply.github.com>
2022-04-20 22:15:45 +0100

    Code Cleanup NO Function Changes

--- routes/sql/sql.js
+++ routes/sql/sql.js
@@ -17,7 +17,6 @@ router
                 res.json(rows)
         ***REMOVED***)
     ***REMOVED*** else if (req.query.matchid) {
-            console.log(req.query.matchid)
             pool.query('SELECT * FROM MatchHistory WHERE MatchID = ?', req.query.matchid.toString(), (err, rows) => {
                 if (err) { res.sendStatus(404); return ***REMOVED***
                 res.json(rows)
