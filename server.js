0bb8a8d2d58e73c8a50bfe59777cd9a9dfdcdee2
Null-Cat <20419682+Null-Cat@users.noreply.github.com>
2022-04-20 18:23:56 +0100

    Added Pug for Page Rendering

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const http = require('http')
 const app = express()
 
 const port = process.env.PORT || 3000
 
 //Routers
 const sql = require('./routes/sql/sql')
+
+app.set('view engine', 'pug')
+app.set('views', './views')
 
 app.use(LogConnections)
 
@@ -13,9 +17,19 @@ app.use('/sql', sql)
 
 app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+  http.get(`http://localhost:${port***REMOVED***/sql?request=matchhistory`, (response) => {
+    let body = ''
+    response.on('data', (chunk) => { body += chunk ***REMOVED***)
+    response.on('end', () => {
+      res.render('index', { matches: JSON.parse(body) ***REMOVED***)
+***REMOVED***)
+  ***REMOVED***)
+***REMOVED***)
+
 function LogConnections(req, res, next) {
-    console.log(`${req.method***REMOVED*** request for ${req.url***REMOVED*** from ${req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.socket.remoteAddress***REMOVED***`)
-    next()
+  console.log(`${req.method***REMOVED*** request for ${req.url***REMOVED*** from ${req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.socket.remoteAddress***REMOVED***`)
+  next()
 ***REMOVED***
 
 app.listen(port, () => {
