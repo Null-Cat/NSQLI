dcdce49ba88fd8fc04f13f80d7c4dc64c0a09341
Null-Cat <20419682+Null-Cat@users.noreply.github.com>
2022-05-07 23:52:31 +0100

    Updated Formatting for Consistency

--- routes/sql/sql.js
+++ routes/sql/sql.js
@@ -10,62 +10,62 @@ const jwtSecret = 'SnowD0wnVERYsEcReTPaSSw0Rd'
 const router = express.Router()
 
 router
-    .route('/')
-    .get((req, res) => {
-        if (req.query.request && req.query.request.toLowerCase() === 'matchhistory') {
-            pool.query('SELECT * FROM MatchHistory ORDER BY EndTime DESC', (err, rows) => {
-                if (err) { res.sendStatus(404); return ***REMOVED***
-                res.json(rows)
-        ***REMOVED***)
-    ***REMOVED*** else if (req.query.request && req.query.request.toLowerCase() === 'leaderboard') {
-            pool.query('SELECT Player1User AS Username, Player1FinalScore AS Score, Player1HeadShotCount AS Headshots, EndTime AS ScoreSetAt, MatchID FROM MatchHistory UNION ALL SELECT Player2User AS Username, Player2FinalScore AS Score, Player2HeadShotCount AS Headshots, EndTime AS ScoreSetAt, MatchID FROM MatchHistory ORDER BY Score DESC',
-                (err, rows) => {
-                    if (err) { res.sendStatus(404); return ***REMOVED***
-                    res.json(rows)
-            ***REMOVED***)
-    ***REMOVED*** else if (req.query.matchid) {
-            pool.query('SELECT * FROM MatchHistory WHERE MatchID = ?', req.query.matchid.toString(), (err, rows) => {
-                if (err) { res.sendStatus(404); return ***REMOVED***
-                res.json(rows)
-        ***REMOVED***)
-    ***REMOVED*** else {
-            res.sendStatus(404)
-    ***REMOVED***
-***REMOVED***)
-    .post(authenticateJWT, express.json(), (req, res) => {
-        if (req.body.cmd === 'add') {
-            pool.query('INSERT INTO MatchHistory SET ?', req.body.data, (err, result) => {
-                if (err && err.code === 'ER_DUP_ENTRY') {
-                    console.log(`${clc.red('Duplicate entry')***REMOVED***`)
-                    res.sendStatus(409)
-                    return
-            ***REMOVED***
-                if (err) throw err
-                res.json(result)
-                console.log(`${clc.green('Added New Match: ')***REMOVED***${clc.cyan.bold(req.body.data.MatchID)***REMOVED***`)
-        ***REMOVED***)
-    ***REMOVED***
-***REMOVED***)
+	.route('/')
+	.get((req, res) => {
+		if (req.query.request && req.query.request.toLowerCase() === 'matchhistory') {
+			pool.query('SELECT * FROM MatchHistory ORDER BY EndTime DESC', (err, rows) => {
+				if (err) { res.sendStatus(404); return ***REMOVED***
+				res.json(rows)
+			***REMOVED***)
+		***REMOVED*** else if (req.query.request && req.query.request.toLowerCase() === 'leaderboard') {
+			pool.query('SELECT Player1User AS Username, Player1FinalScore AS Score, Player1HeadShotCount AS Headshots, EndTime AS ScoreSetAt, MatchID FROM MatchHistory UNION ALL SELECT Player2User AS Username, Player2FinalScore AS Score, Player2HeadShotCount AS Headshots, EndTime AS ScoreSetAt, MatchID FROM MatchHistory ORDER BY Score DESC',
+				(err, rows) => {
+					if (err) { res.sendStatus(404); return ***REMOVED***
+					res.json(rows)
+				***REMOVED***)
+		***REMOVED*** else if (req.query.matchid) {
+			pool.query('SELECT * FROM MatchHistory WHERE MatchID = ?', req.query.matchid.toString(), (err, rows) => {
+				if (err) { res.sendStatus(404); return ***REMOVED***
+				res.json(rows)
+			***REMOVED***)
+		***REMOVED*** else {
+			res.sendStatus(404)
+		***REMOVED***
+	***REMOVED***)
+	.post(authenticateJWT, express.json(), (req, res) => {
+		if (req.body.cmd === 'add') {
+			pool.query('INSERT INTO MatchHistory SET ?', req.body.data, (err, result) => {
+				if (err && err.code === 'ER_DUP_ENTRY') {
+					console.log(`${clc.red('Duplicate entry')***REMOVED***`)
+					res.sendStatus(409)
+					return
+				***REMOVED***
+				if (err) throw err
+				res.json(result)
+				console.log(`${clc.green('Added New Match: ')***REMOVED***${clc.cyan.bold(req.body.data.MatchID)***REMOVED***`)
+			***REMOVED***)
+		***REMOVED***
+	***REMOVED***)
 
 function authenticateJWT(req, res, next) {
-    const authHeader = req.headers.authorization
+	const authHeader = req.headers.authorization
 
-    if (authHeader) {
-        const token = authHeader.split(' ')[1]
+	if (authHeader) {
+		const token = authHeader.split(' ')[1]
 
-        jwt.verify(token, jwtSecret, (err, user) => {
-            if (err) {
-                console.log(`${clc.red('Invalid/Unauthorized Token')***REMOVED***`)
-                return res.sendStatus(403)
-        ***REMOVED***
-            console.log(user)
-            req.user = user
-            next()
-    ***REMOVED***)
-***REMOVED*** else {
-        console.log(`${clc.red('Not Authenticated')***REMOVED***`)
-        res.sendStatus(401)
-***REMOVED***
+		jwt.verify(token, jwtSecret, (err, user) => {
+			if (err) {
+				console.log(`${clc.red('Invalid/Unauthorized Token')***REMOVED***`)
+				return res.sendStatus(403)
+			***REMOVED***
+			console.log(user)
+			req.user = user
+			next()
+		***REMOVED***)
+	***REMOVED*** else {
+		console.log(`${clc.red('Not Authenticated')***REMOVED***`)
+		res.sendStatus(401)
+	***REMOVED***
 ***REMOVED***
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,65 +1,80 @@
-const express = require('express')
-const http = require('http')
-const clc = require('cli-color')
-const argv = require('yargs')(process.argv.slice(2))
-  .command('dev', 'Run the server in development mode')
-  .alias('dev', 'd')
-  .argv
+const express = require("express")
+const http = require("http")
+const clc = require("cli-color")
+const argv = require("yargs")(process.argv.slice(2))
+	.command("dev", "Run the server in development mode")
+	.alias("dev", "d").argv
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
 //Routers
-const sql = require('./routes/sql/sql')
+const sql = require("./routes/sql/sql")
 
-app.set('view engine', 'pug')
-app.set('views', './views')
+app.set("view engine", "pug")
+app.set("views", "./views")
 
 app.use(LogConnections)
 
-app.enable('trust proxy')
-app.all('*', (req, res, next) => {
-  var isLocal = (req.socket.localAddress === req.socket.remoteAddress)
-  if (!argv.dev && !req.secure && !isLocal) res.redirect(308, 'https://' + req.headers.host + req.url)
-  else next()
+app.enable("trust proxy")
+app.all("*", (req, res, next) => {
+	var isLocal = req.socket.localAddress === req.socket.remoteAddress
+	if (!argv.dev && !req.secure && !isLocal)
+		res.redirect(308, "https://" + req.headers.host + req.url)
+	else next()
 ***REMOVED***)
 
 app.use(express.urlencoded({ extended: true ***REMOVED***))
-app.use('/sql', sql)
+app.use("/sql", sql)
 
-app.use(express.static('public'))
+app.use(express.static("public"))
 
-app.get('/', (req, res) => {
-  http.get(`http://localhost:${port***REMOVED***/sql?request=matchhistory`, (response) => {
-    let body = ''
-    response.on('data', (chunk) => { body += chunk ***REMOVED***)
-    response.on('end', () => {
-      res.render('index', { matches: JSON.parse(body) ***REMOVED***)
-***REMOVED***)
-  ***REMOVED***)
-***REMOVED***)
-
-app.get('/leaderboard', (req, res) => {
-  http.get(`http://localhost:${port***REMOVED***/sql?request=leaderboard`, (response) => {
-    let body = ''
-    response.on('data', (chunk) => { body += chunk ***REMOVED***)
-    response.on('end', () => {
-      res.render('leaderboard', { leaderboardData: JSON.parse(body) ***REMOVED***)
-***REMOVED***)
-  ***REMOVED***)
+app.get("/", (req, res) => {
+	http.get(`http://localhost:${port***REMOVED***/sql?request=matchhistory`, (response) => {
+		let body = ""
+		response.on("data", (chunk) => {
+			body += chunk
+		***REMOVED***)
+		response.on("end", () => {
+			res.render("index", { matches: JSON.parse(body) ***REMOVED***)
+		***REMOVED***)
+	***REMOVED***)
 ***REMOVED***)
 
-app.all('*', (req, res) => {
-  res.sendStatus(404)
-***REMOVED***);
+app.get("/leaderboard", (req, res) => {
+	http.get(`http://localhost:${port***REMOVED***/sql?request=leaderboard`, (response) => {
+		let body = ""
+		response.on("data", (chunk) => {
+			body += chunk
+		***REMOVED***)
+		response.on("end", () => {
+			res.render("leaderboard", { leaderboardData: JSON.parse(body) ***REMOVED***)
+		***REMOVED***)
+	***REMOVED***)
+***REMOVED***)
+
+app.all("*", (req, res) => {
+	res.sendStatus(404)
+***REMOVED***)
 
 function LogConnections(req, res, next) {
-  console.log(`${clc.inverse(req.method)***REMOVED*** request for ${clc.underline(req.url)***REMOVED*** from ${clc.cyan(req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.socket.remoteAddress)***REMOVED***`)
-  next()
+	console.log(
+		`${clc.inverse(req.method)***REMOVED*** request for ${clc.underline(
+			req.url
+		)***REMOVED*** from ${clc.cyan(
+			req.headers["x-forwarded-for"]
+				? req.headers["x-forwarded-for"].split(",")[0]
+				: req.socket.remoteAddress
+		)***REMOVED***`
+	)
+	next()
 ***REMOVED***
 
 app.listen(port, () => {
-  if (argv.dev) console.log(`${clc.yellow(`Server in development mode ${clc.bold('NO SSL')***REMOVED***`)***REMOVED***`)
-  console.log(`${clc.magentaBright(`Listening on port ${port***REMOVED***`)***REMOVED***`)
+	if (argv.dev)
+		console.log(
+			`${clc.yellow(`Server in development mode ${clc.bold("NO SSL")***REMOVED***`)***REMOVED***`
+		)
+	console.log(`${clc.magentaBright(`Listening on port ${port***REMOVED***`)***REMOVED***`)
 ***REMOVED***)
